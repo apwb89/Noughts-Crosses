907e00d80e6bea669eaac4bdbaf92571ab2a69b8
Dane <danejwhitfield@gmail.com>
2022-04-04 15:54:37 +0100

    made it look a bit prettier

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
-const divGridElement = [...document.querySelectorAll('div')];
+const divGridElement = [...document.querySelectorAll("div")];
 const divInnerText = divGridElement.map((x) => x.innerText);
-const winnerText = document.querySelector('.winner');
+const winnerText = document.querySelector(".winner");
 
 const startGame = () => {
-    let isWinner = false;
+  let isWinner = false;
 
-  let currentTurn = 'X';
+  let currentTurn = "✘";
   const changeTurn = () => {
-    if (currentTurn === 'X') {
-      currentTurn = 'O';
+    if (currentTurn === "✘") {
+      currentTurn = "☮";
     } else {
-      currentTurn = 'X';
+      currentTurn = "✘";
     }
   };
 
   let arr = [];
 
   const checkWinningLine = () => {
-
     const winners = [
       [0, 1, 2],
       [0, 3, 6],
@@ -29,31 +28,35 @@ const startGame = () => {
       [2, 4, 6],
     ];
 
-    if(isWinner === false) {
-        winners.forEach((winner) => {
-          winner.forEach(() => {
-            const firstValue = arr[winner[0]];
-            if (firstValue) {
-              if (firstValue === arr[winner[1]] && firstValue === arr[winner[2]]) {
-                winnerText.innerText = `${firstValue} wins!`;
-                isWinner = true;
-                divGridElement[winner[0]].style.background = "#90EE90";
-                divGridElement[winner[1]].style.background = "#90EE90";
-                divGridElement[winner[2]].style.background = "#90EE90";
-              }
+    if (isWinner === false) {
+      winners.forEach((winner) => {
+        winner.forEach(() => {
+          const firstValue = arr[winner[0]];
+          if (firstValue) {
+            if (
+              firstValue === arr[winner[1]] &&
+              firstValue === arr[winner[2]]
+            ) {
+              winnerText.innerText = `${firstValue} wins!`;
+              isWinner = true;
+              divGridElement[winner[0]].style.background = "#90EE90";
+              divGridElement[winner[1]].style.background = "#90EE90";
+              divGridElement[winner[2]].style.background = "#90EE90";
             }
-          });
+          }
         });
+      });
     }
   };
 
   divGridElement.forEach((div, i) =>
-    div.addEventListener('click', () => {
+    div.addEventListener("click", () => {
       if (!div.innerText) {
         div.innerText = currentTurn;
         arr[i] = div.innerText;
         changeTurn();
         checkWinningLine();
+        makeMove();
       }
     })
   );
